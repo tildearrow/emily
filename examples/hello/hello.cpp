b6474db630271e7dcd7f9e2ba82d28386cbaa423
tildearrow <tildearrow@users.noreply.github.com>
2017-11-12 03:33:44 -0500

    example tweaks

--- examples/hello/hello.cpp
+++ examples/hello/hello.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <unistd.h>
 #include "toolkit.h"
 
@@ -7,13 +8,15 @@ eFrame* frame;
 eLabel* hello;
 eButton* but;
 
-void myCallback(const eEvent* ev) {
-  hello->x=ev->coord.x;
-  hello->y=ev->coord.y;
-}
+unsigned char sineVal;
 
 void myOtherCallback() {
   printf("hello world!\n");
+}
+
+void sineCallback() {
+  sineVal++;
+  hello->x=210+sin(2*M_PI*((float)sineVal/256))*80;
 }
 
 int main(int argc, char** argv) {
@@ -23,15 +26,15 @@ int main(int argc, char** argv) {
   hello=(eLabel*)frame->newWidget<eLabel>();
   hello->setString("hello world!");
   hello->x=64;
-  hello->y=200;
+  hello->y=64;
   but=(eButton*)frame->newWidget<eButton>();
   but->setLabel("Button");
   but->setColor({32,32,32,255});
   but->setSize(80,32);
   but->setCallback(myOtherCallback);
-  but->x=64;
+  but->x=200;
   but->y=200;
-  gui->setPostEventCallback(eEventMouseMove,myCallback);
+  gui->setPreDrawCallback(sineCallback);
   gui->pushFrame(frame);
   return gui->run();
 }
