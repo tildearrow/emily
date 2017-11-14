597accfbf1c3caf9d53ef9720f55be3930f52089
tildearrow <tildearrow@users.noreply.github.com>
2017-11-13 19:42:23 -0500

    finish high-quality rectangles

--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -33,15 +33,80 @@ void eBitmap::rect(double x, double y, double w, double h, eColor color) {
   ax2=(int)(x+w);
   rbi=(x+w)-(int)(x+w);
   if (ax2>=width) {
-    ax2=width-1; rbi=0;
+    ax2=width; rbi=0;
   }
   ay2=(int)(y+h);
   bbi=(y+h)-(int)(y+h);
   if (ay2>=height) {
-    ay2=height-1; bbi=0;
+    ay2=height; bbi=0;
   }
-  // smoothing to be completed
-  //printf("%f %f %f %f\n",lbi,rbi,tbi,bbi);
+  // left border
+  if (lbi>0) {
+    for (int j=ay1; j<ay2; j++) {
+      data[(j*width+ax1-1)<<2]+=(color.r-data[(j*width+ax1-1)<<2])*(color.a*lbi);
+      data[1+((j*width+ax1-1)<<2)]+=(color.g-data[1+((j*width+ax1-1)<<2)])*(color.a*lbi);
+      data[2+((j*width+ax1-1)<<2)]+=(color.b-data[2+((j*width+ax1-1)<<2)])*(color.a*lbi);
+      data[3+((j*width+ax1-1)<<2)]=(lbi*color.a)+(data[3+((j*width+ax1-1)<<2)]*(1-(color.a*lbi)));
+    }
+  }
+  // right border
+  if (rbi>0) {
+    for (int j=ay1; j<ay2; j++) {
+      data[(j*width+ax2)<<2]+=(color.r-data[(j*width+ax2)<<2])*(color.a*rbi);
+      data[1+((j*width+ax2)<<2)]+=(color.g-data[1+((j*width+ax2)<<2)])*(color.a*rbi);
+      data[2+((j*width+ax2)<<2)]+=(color.b-data[2+((j*width+ax2)<<2)])*(color.a*rbi);
+      data[3+((j*width+ax2)<<2)]=(rbi*color.a)+(data[3+((j*width+ax2)<<2)]*(1-(color.a*rbi)));
+    }
+  }
+  // top border
+  if (tbi>0) {
+    for (int i=ax1; i<ax2; i++) {
+      data[((ay1-1)*width+i)<<2]+=(color.r-data[((ay1-1)*width+i)<<2])*(color.a*tbi);
+      data[1+(((ay1-1)*width+i)<<2)]+=(color.g-data[1+(((ay1-1)*width+i)<<2)])*(color.a*tbi);
+      data[2+(((ay1-1)*width+i)<<2)]+=(color.b-data[2+(((ay1-1)*width+i)<<2)])*(color.a*tbi);
+      data[3+(((ay1-1)*width+i)<<2)]=(color.a*tbi)+(data[3+(((ay1-1)*width+i)<<2)]*(1-(color.a*tbi)));
+    }
+  }
+  // bottom border
+  if (bbi>0) {
+    for (int i=ax1; i<ax2; i++) {
+      data[(ay2*width+i)<<2]+=(color.r-data[(ay2*width+i)<<2])*(color.a*bbi);
+      data[1+((ay2*width+i)<<2)]+=(color.g-data[1+((ay2*width+i)<<2)])*(color.a*bbi);
+      data[2+((ay2*width+i)<<2)]+=(color.b-data[2+((ay2*width+i)<<2)])*(color.a*bbi);
+      data[3+((ay2*width+i)<<2)]=(color.a*bbi)+(data[3+((ay2*width+i)<<2)]*(1-(color.a*bbi)));
+    }
+  }
+  
+  // corners
+  // left top
+  if (lbi>0 && tbi>0) {
+    data[((ay1-1)*width+ax1-1)<<2]+=(color.r-data[((ay1-1)*width+ax1-1)<<2])*(color.a*lbi*tbi);
+    data[1+(((ay1-1)*width+ax1-1)<<2)]+=(color.g-data[1+(((ay1-1)*width+ax1-1)<<2)])*(color.a*lbi*tbi);
+    data[2+(((ay1-1)*width+ax1-1)<<2)]+=(color.b-data[2+(((ay1-1)*width+ax1-1)<<2)])*(color.a*lbi*tbi);
+    data[3+(((ay1-1)*width+ax1-1)<<2)]=(color.a*lbi*tbi)+(data[3+(((ay1-1)*width+ax1-1)<<2)]*(1-(color.a*lbi*tbi)));
+  }
+  // right top
+  if (rbi>0 && tbi>0) {
+    data[((ay1-1)*width+ax2)<<2]+=(color.r-data[((ay1-1)*width+ax2)<<2])*(color.a*rbi*tbi);
+    data[1+(((ay1-1)*width+ax2)<<2)]+=(color.g-data[1+(((ay1-1)*width+ax2)<<2)])*(color.a*rbi*tbi);
+    data[2+(((ay1-1)*width+ax2)<<2)]+=(color.b-data[2+(((ay1-1)*width+ax2)<<2)])*(color.a*rbi*tbi);
+    data[3+(((ay1-1)*width+ax2)<<2)]=(color.a*rbi*tbi)+(data[3+(((ay1-1)*width+ax2)<<2)]*(1-(color.a*rbi*tbi)));
+  }
+  // left bottom
+  if (lbi>0 && bbi>0) {
+    data[(ay2*width+ax1-1)<<2]+=(color.r-data[(ay2*width+ax1-1)<<2])*(color.a*rbi*tbi);
+    data[1+((ay2*width+ax1-1)<<2)]+=(color.g-data[1+((ay2*width+ax1-1)<<2)])*(color.a*rbi*tbi);
+    data[2+((ay2*width+ax1-1)<<2)]+=(color.b-data[2+((ay2*width+ax1-1)<<2)])*(color.a*rbi*tbi);
+    data[3+((ay2*width+ax1-1)<<2)]=(color.a*rbi*tbi)+(data[3+((ay2*width+ax1-1)<<2)]*(1-(color.a*rbi*tbi)));
+  }
+  // right bottom
+  if (rbi>0 && bbi>0) {
+    data[(ay2*width+ax2)<<2]+=(color.r-data[(ay2*width+ax2)<<2])*(color.a*rbi*bbi);
+    data[1+((ay2*width+ax2)<<2)]+=(color.g-data[1+((ay2*width+ax2)<<2)])*(color.a*rbi*bbi);
+    data[2+((ay2*width+ax2)<<2)]+=(color.b-data[2+((ay2*width+ax2)<<2)])*(color.a*rbi*bbi);
+    data[3+((ay2*width+ax2)<<2)]=(color.a*rbi*bbi)+(data[3+((ay2*width+ax2)<<2)]*(1-(color.a*rbi*bbi)));
+  }
+  
   if (color.a>=1) {
     for (int j=ay1; j<ay2; j++) {
       for (int i=ax1; i<ax2; i++) {
