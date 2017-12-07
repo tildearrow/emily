a3c82d1178d685934ccc27cb8610d17f3cc83f53
tildearrow <tildearrow@users.noreply.github.com>
2017-12-06 19:36:43 -0500

    tweak hello program

--- examples/hello/hello.cpp
+++ examples/hello/hello.cpp
@@ -8,6 +8,7 @@ eFrame* frame;
 eFrame* frame1;
 eLabel* hello;
 eButton* but;
+eButton* butf;
 eFrameView* fview;
 eButton* but1;
 eButton* but2;
@@ -17,9 +18,18 @@ unsigned char sineVal;
 
 double value;
 
+eColor getRandomColor() {
+  return {(float)(rand()&0xff)/256,(float)(rand()&0xff)/256,(float)(rand()&0xff)/256,1};
+}
+
 void myOtherCallback() {
   printf("hello world!\n");
-  but->setColor({(float)(rand()&0xff)/256,(float)(rand()&0xff)/256,(float)(rand()&0xff)/256,1});
+  but->setColor(getRandomColor());
+}
+
+void myOtherCallbackF() {
+  printf("hello world!\n");
+  butf->setColor(getRandomColor());
 }
 
 void oneCallback() {
@@ -37,26 +47,40 @@ void sineCallback() {
 
 int main(int argc, char** argv) {
   gui=new eEngine(480,320);
+  srand(gui->perfCount());
   gui->show();
   frame=gui->newFrame();
+  
   hello=frame->newWidget<eLabel>();
   hello->setString("hello world!");
   hello->x=64;
   hello->y=64;
+  
   but=frame->newWidget<eButton>();
-  but->setLabel("Button");
-  but->setColor({0,0.75,1,1});
+  but->setLabel("Normal");
+  but->setColor(getRandomColor());
   but->setSize(80,32);
-  but->setStyle(eButtonFlat);
   but->setCallback(myOtherCallback);
-  but->x=200;
+  but->x=100;
   but->y=200;
+  
+  butf=frame->newWidget<eButton>();
+  butf->setLabel("Flat");
+  butf->setColor(getRandomColor());
+  butf->setSize(80,32);
+  butf->setStyle(eButtonFlat);
+  butf->setCallback(myOtherCallbackF);
+  butf->x=300;
+  butf->y=200;
+  
   slider=frame->newWidget<eSlider>();
   slider->setTrack(value);
   slider->setRange(-1,1);
   slider->setHandleColor({0,0.75,1,1});
   slider->setHandleSize(6);
+  
   frame1=gui->newFrame();
+  
   but1=frame1->newWidget<eButton>();
   but1->setColor({1,1,0,1});
   but1->setSize(64,32);
@@ -64,6 +88,7 @@ int main(int argc, char** argv) {
   but1->setCallback(oneCallback);
   but1->x=4;
   but1->y=4;
+  
   but2=frame1->newWidget<eButton>();
   but2->setColor({1,1,0,1});
   but2->setSize(64,32);
@@ -71,11 +96,13 @@ int main(int argc, char** argv) {
   but2->setCallback(twoCallback);
   but2->x=72;
   but2->y=4;
+  
   fview=frame->newWidget<eFrameView>();
   fview->setSize(256,40);
   fview->pushFrame(frame1);
   fview->x=32;
   fview->y=32;
+  
   gui->setPreDrawCallback(sineCallback);
   gui->pushFrame(frame);
   return gui->run();
