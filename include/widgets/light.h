035afec4cd9eadd1334e18616095ca549d7677f2
tildearrow <tildearrow@users.noreply.github.com>
2018-03-06 17:25:38 -0500

    prepare for light widget

---
+++ include/widgets/light.h
@@ -0,0 +1,35 @@
+enum eLightMaterials {
+  eLightPlastic=0,
+  eLightRubber=1
+};
+
+class eLight: public eWidget {
+  string label;
+  eIcon* icon;
+  sf::Text* linst;
+  sf::Texture* tinst;
+  sf::Sprite sinst;
+  sf::Sprite sinstLight;
+  double hue, saturation;
+  float bright;
+  float highlight;
+  void (*leftClickCallback)();
+  double xo, yo;
+  int fw, fh;
+  int bstyle;
+  bool clicked;
+  int atrList[8];
+  char iconPlace;
+  public:
+    XPT int init();
+    XPT int setLabel(string data);
+    XPT int setLabelSize(double size);
+    XPT int setIcon(eIcons index, double size, eDirection placement);
+    XPT int setSize(double w, double h);
+    XPT int setStyle(eButtonStyles style);
+    XPT int setColor(eColor col);
+    XPT int event(eEvent& ev);
+    XPT int setCallback(void (*callback)());
+    XPT int update();
+    XPT int draw();
+};
