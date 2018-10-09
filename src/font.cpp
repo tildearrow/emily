b651976db6fbb55edc611d003fdda185067d2331
tildearrow <tildearrow@users.noreply.github.com>
2018-10-09 01:50:26 -0500

    better font search method

--- src/font.cpp
+++ src/font.cpp
@@ -4,6 +4,53 @@ extern "C" {
 #include "nsstub.h"
 }
 #endif
+#include <string.h>
+
+// font names. sorted from best to worst
+const char* fontNames[]={
+  // try with sans-serif
+  "sans-serif",
+  // GOT YOU, DEJAVU USER!
+  // this time I am not obeying your orders!
+  "IBM Plex Sans",
+  "Ubuntu",
+  "Helvetica",
+  "FreeSans",
+  // to be honest I didn't want to Android-ize
+  "Roboto",
+  // wait, you really don't have Roboto? cool.
+  "Oxygen Sans",
+  "Oxygen-Sans",
+  "Cantarell",
+  "Noto Sans",
+  "Droid Sans",
+  "Linux Biolinum",
+  "Liberation Sans",
+  "Arimo",
+  // *sigh* at least you may have windows fonts, huh?
+  "Segoe UI",
+  "Calibri",
+  "Arial",
+  // oh come on at least Tahoma but NOT DejaVu, ever
+  "Tahoma",
+  "Verdana",
+  // even comic sans is better
+  "Comic Sans",
+  "Comic Sans MS",
+  "Comic Neue",
+  // ARGH I GIVE UP!
+  // but I do wanna rant about this font!
+  // DejaVu is the worst font in history! it is insanely obese,
+  // when compared to the entire rest of fonts in other OS'es.
+  // I'm especially sad how Purism is using it for their Librem
+  // phone, since it is a waste of scarce horizontal space!!!
+  // please do *NOT* use DejaVu in your system or next project.
+  // otherwise it will look dated as hell and ugly.
+  //
+  // I tried to not swear...
+  "DejaVu Sans",
+  NULL
+};
 
 int eFont::loaddef(int variant) {
   string path;
@@ -11,13 +58,13 @@ int eFont::loaddef(int variant) {
   FcPattern* fmpattern;
   FcPattern* match;
   FcResult fmresult;
-  FcValue sans;
+  FcValue fontName;
   FcValue regular;
   FcValue resval;
 #endif
-  // TODO: kill this ugly default font
   switch (variant) {
     case eFontDefault:
+    case eFontLarge:
 #if defined(_WIN32)
       // windows font code here
       path="C:\\Windows\\Fonts\\segoeui.ttf";
@@ -28,22 +75,33 @@ int eFont::loaddef(int variant) {
 #elif defined(__unix__)
       // fontconfig font code here
 #if defined(HAVE_FONTCONFIG)
-      sans.type=FcTypeString;
-      sans.u.s=(const FcChar8*)"sans-serif";
+      fontName.type=FcTypeString;
       regular.type=FcTypeString;
       regular.u.s=(const FcChar8*)"Regular";
-      fmpattern=FcPatternCreate();
-      FcPatternAdd(fmpattern,FC_FAMILY,sans,FcTrue);
-      FcPatternAdd(fmpattern,FC_STYLE,regular,FcTrue);
       if (!FcInit()) {
         eLogE("Error while initializing fontconfig.\n");
       }
-      FcConfigSubstitute(NULL,fmpattern,FcMatchPattern);
-      match=FcFontMatch(NULL,fmpattern,&fmresult);
-      FcPatternGet(match,FC_FILE,0,&resval);
-      path=(const char*)resval.u.s;
-      FcPatternDestroy(fmpattern);
-      FcPatternDestroy(match);
+      
+      for (int i=0; fontNames[i]!=NULL; i++) {
+        fontName.u.s=(const FcChar8*)"sans-serif";
+        fmpattern=FcPatternCreate();
+        FcPatternAdd(fmpattern,FC_FAMILY,fontName,FcTrue);
+        FcPatternAdd(fmpattern,FC_STYLE,regular,FcTrue);
+        FcConfigSubstitute(NULL,fmpattern,FcMatchPattern);
+        match=FcFontMatch(NULL,fmpattern,&fmresult);
+        if (match==NULL) continue;
+        FcPatternGet(match,FC_FILE,0,&resval);
+        path=(const char*)resval.u.s;
+        if (strstr(path.c_str(),"DejaVu")!=NULL && fontNames[i+1]!=NULL) {
+          printf("caught you usin' DejaVu!\n");
+          FcPatternDestroy(fmpattern);
+          FcPatternDestroy(match);
+          continue;
+        }
+        FcPatternDestroy(fmpattern);
+        FcPatternDestroy(match);
+        break;
+      }
 #else
       eLogE("Wow! Really? No fontconfig library!\n");
 #endif
@@ -51,42 +109,6 @@ int eFont::loaddef(int variant) {
 #else
       // assume /font.ttf
       path="/font.ttf";
-#endif
-      break;
-    case eFontLarge:
-      // DejaF*ck
-#if defined(_WIN32)
-      // windows font code here
-      path="C:\\Windows\\Fonts\\segoeui.ttf";
-#elif defined(__APPLE__)
-      // apple font code here
-      path="/System/Library/Fonts/Helvetica.dfont";
-#elif defined(__unix__)
-      // fontconfig font code here
-#if defined(HAVE_FONTCONFIG)
-      sans.type=FcTypeString;
-      sans.u.s=(const FcChar8*)"sans-serif";
-      regular.type=FcTypeString;
-      regular.u.s=(const FcChar8*)"Regular";
-      fmpattern=FcPatternCreate();
-      FcPatternAdd(fmpattern,FC_FAMILY,sans,FcTrue);
-      FcPatternAdd(fmpattern,FC_STYLE,regular,FcTrue);
-      if (!FcInit()) {
-        eLogE("Error while initializing fontconfig.\n");
-      }
-      FcConfigSubstitute(NULL,fmpattern,FcMatchPattern);
-      match=FcFontMatch(NULL,fmpattern,&fmresult);
-      FcPatternGet(match,FC_FILE,0,&resval);
-      path=(const char*)resval.u.s;
-      FcPatternDestroy(fmpattern);
-      FcPatternDestroy(match);
-#else
-      eLogE("Wow! Really? No fontconfig library!\n");
-#endif
-      break;
-#else
-      // assume /fontLarge.ttf
-      path="/fontLarge.ttf";
 #endif
       break;
     default:
